7a6d5e9d7cad33a8962493ba9673993126dde6cd
Anthony Lionetti <lionetti.tech@gmail.com>
2025-01-28 17:02:44 -0500

    add theme context

---
+++ frontend/context/ThemeContext.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { Theme } from "@radix-ui/themes";
+
+interface ThemeContextType {
+    theme: "light" | "dark";
+    setTheme: (theme: "light" | "dark") => void;
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+    theme: "dark",
+    setTheme: () => {}
+})
+
+export function ThemeProvider({children}:{children:React.ReactNode}) {
+    // set a theme to what the system saved, or sets it to dark
+    const [theme, setTheme] = useState<"light"|"dark">(() => {
+        const savedTheme = localStorage.getItem('theme') as "light" | "dark"
+        return savedTheme
+    })
+
+    // update localStorage whenever theme changes
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme])
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            <Theme appearance={theme} accentColor="iris" grayColor="slate">
+                {children}
+            </Theme>
+        </ThemeContext.Provider>
+    )
+}
+
+export function useTheme(): ThemeContextType {
+    return useContext(ThemeContext);
+}
