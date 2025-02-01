15ab778e63b6de9c1865ff8e8ef742ca6cd06832
Anthony Lionetti <lionetti.tech@gmail.com>
2025-02-01 09:39:14 -0500

    update header

--- frontend/components/ chat/ChatWrapper.tsx
+++ frontend/components/ chat/ChatWrapper.tsx
@@ -6,7 +6,7 @@ export default function ChatWrapper() {
   return (
     <div className="mx-auto fixed top-0 left-0 right-0">
       <div className="text-center py-4 mb-auto">
-        <Heading color="blue">Chatbot</Heading>
+        <h3 className="text-xl font-semibold text-accent-8">Stealth Chatbot</h3>
       </div>
       <div className="flex flex-col max-h-[80dvh] overflow-auto">
         <ChatMessages />
