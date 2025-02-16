e32353bd6d23e4ae2be4aee76677a65b5bd03c3d
Anthony Lionetti <lionetti.tech@gmail.com>
2025-02-15 23:29:23 -0500

    improve sidebar UX

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,70 +1,65 @@
 "use client";
 import { useState } from "react";
 import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
+  ChevronUpIcon,
+  PinLeftIcon,
+  PinRightIcon,
   PlusIcon,
 } from "@radix-ui/react-icons";
-import Image from "next/image";
 import Link from "next/link";
-import { Avatar, Card, IconButton } from "@radix-ui/themes";
+import { Avatar, Card, Tooltip } from "@radix-ui/themes";
 
 export const Sidebar = () => {
   const conversations = useMockHistory();
-  const [isOpen, setIsOpen] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isPinned, setIsPinned] = useState(false);
 
-  const toggleSidebar = () => setIsOpen(!isOpen);
+  const toggleSidebar = () => setIsHovered(!isHovered);
+
+  const isOpen = isHovered || isPinned;
 
   return (
     <div
       className={`
-      fixed h-screen bg-gray-2 border-r border-gray-5
+      fixed h-screen bg-gray-2
       transition-all duration-300 ease-in-out z-10
-      ${isOpen ? "w-64" : "w-20"}
+      ${isOpen ? "w-64" : "w-0"}
     `}
+      onMouseEnter={toggleSidebar}
+      onMouseLeave={toggleSidebar}
     >
-      {/* Toggle Button */}
-      <button
-        onClick={toggleSidebar}
-        className="absolute -right-4 top-5 bg-gray-1 rounded-full p-1.5 
-                 shadow-md border border-gray-5 hover:bg-gray-2 z-10"
-      >
-        {isOpen ? (
-          <ChevronLeftIcon className="h-4 w-4 text-gray-12" />
-        ) : (
-          <ChevronRightIcon className="h-4 w-4 text-gray-12" />
-        )}
-      </button>
-
-      {/* Content */}
-      <div className="p-4 h-full flex flex-col items-center">
+      <div className="p-4 h-full flex flex-col items-start">
         {/* Header */}
-        <Link href="/" className="flex flex-row gap-1 w-auto items-end mb-8">
-          <Image
-            src={"/logo.png"}
-            alt="logo"
-            height={36}
-            width={48}
-            className="w-12 h-8 inline-block"
-          />
-          {isOpen && (
-            <h2 className="text-left text-xl font-bold text-gray-12">
-              Stealth Chatbot
+        <div className="flex w-full">
+          <Link
+            href="/"
+            className="flex flex-1 flex-row gap-1 w-auto items-end mb-12"
+          >
+            <h2 className="top-4 text-left text-xl font-bold text-gray-12">
+              Chatbot
             </h2>
-          )}
-        </Link>
+          </Link>
 
-        {isOpen ? (
-          <Card className="flex w-full bg-accent-8 flex-row gap-3 items-center p-1 transition-color duration-300 ease-in-out hover:cursor-pointer hover:bg-accent-7">
-            <PlusIcon className="w-4 h-4" />
-            {isOpen && (
-              <p className="font-semibold text-md">Start a New Chat</p>
-            )}
+          {/* Pin Sidebar */}
+          {isOpen && (
+            <Tooltip content="Pin taskbar">
+              <button
+                onClick={() => setIsPinned(!isPinned)}
+                className="h-6 w-6 p-1 rounded-md bg-gray-5"
+              >
+                {isPinned ? <PinLeftIcon /> : <PinRightIcon />}
+              </button>
+            </Tooltip>
+          )}
+        </div>
+
+        {isOpen && (
+          <Card
+            className={`flex w-full bg-accent-8 flex-row gap-3 items-center px-2 py-1 transition-color duration-300 ease-in-out hover:cursor-pointer hover:bg-accent-7`}
+          >
+            <PlusIcon className="w-5 h-5 font-bold" />
+            <p className="font-semibold text-md">New Chat</p>
           </Card>
-        ) : (
-          <IconButton size={"2"} className="bg-accent-8">
-            <PlusIcon className="w-4 h-4" />
-          </IconButton>
         )}
         {/* Conversation List */}
         <div className="flex-1 space-y-1 overflow-y-auto mt-12">
@@ -95,14 +90,17 @@ export const Sidebar = () => {
         {/* Footer */}
         <div className="mt-4">
           {isOpen ? (
-            <Card className="flex gap-2 items-center transition-colors duration-300 ease-in-out hover:bg-gray-4 hover:cursor-pointer">
+            <Card className="flex gap-2 p-2 items-center transition-colors duration-300 ease-in-out hover:bg-gray-4 hover:cursor-pointer">
               <Avatar fallback="AL" size={"2"} />
               <span className="text-sm text-gray-11">
                 lionetti.tech@gmail.com
               </span>
+              <ChevronUpIcon />
             </Card>
           ) : (
-            <Avatar fallback="AL" size={"2"} />
+            <div className="p-2">
+              <Avatar fallback="AL" size={"2"} />
+            </div>
           )}
         </div>
       </div>
